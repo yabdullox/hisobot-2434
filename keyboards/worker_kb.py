c01f1a3e221b1f8ebfd28613b24cadcc52949df1
yabdullox <132588376+yabdullox@users.noreply.github.com>
2025-10-26 19:38:06 +0500

    Update worker_kb.py

--- keyboards/worker_kb.py
+++ keyboards/worker_kb.py
@@ -1,22 +1,31 @@
+# from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+
+# # 👷‍♂️ Worker menyusi
+# worker_menu = ReplyKeyboardMarkup(
+#     resize_keyboard=True,
+#     keyboard=[
+#         [
+#             KeyboardButton(text="🧾 Hisobot yuborish"),
+#             KeyboardButton(text="⏰ Ishni boshladim")
+#         ],
+#         [
+#             KeyboardButton(text="🐝 Bonus/Jarimalarim")
+#         ],
+#         [
+#             KeyboardButton(text="📷 Tozalash rasmi yuborish")
+            
+#         ],
+#         [
+#             KeyboardButton(text="🔙 Menyuga qaytish")
+#         ]
+#     ]
+# )
+
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
-# 👷‍♂️ Worker menyusi
-worker_menu = ReplyKeyboardMarkup(
-    resize_keyboard=True,
-    keyboard=[
-        [
-            KeyboardButton(text="🧾 Hisobot yuborish"),
-            KeyboardButton(text="⏰ Ishni boshladim")
-        ],
-        [
-            KeyboardButton(text="🐝 Bonus/Jarimalarim")
-        ],
-        [
-            KeyboardButton(text="📷 Tozalash rasmi yuborish")
-            
-        ],
-        [
-            KeyboardButton(text="🔙 Menyuga qaytish")
-        ]
+def worker_menu():
+    kb = [
+        [KeyboardButton(text="🧾 Hisobot yuborish"), KeyboardButton(text="📅 Bugungi hisobot")],
+        [KeyboardButton(text="💸 Bonus/Jarimalar"), KeyboardButton(text="⚙️ Sozlamalar")]
     ]
-)
+    return ReplyKeyboardMarkup(keyboard=kb, resize_keyboard=True)
