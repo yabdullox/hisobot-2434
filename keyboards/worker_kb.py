e0f7476d5b7e76804ab49523bf83207e721c38bf
yabdullox <132588376+yabdullox@users.noreply.github.com>
2025-10-27 16:55:41 +0500

    Update worker_kb.py

--- keyboards/worker_kb.py
+++ keyboards/worker_kb.py
@@ -2,10 +2,21 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 def worker_menu():
     kb = [
-        [KeyboardButton(text="🧾 Hisobot yuborish"), KeyboardButton(text="⏰ Ishni boshladim")],
-        [KeyboardButton(text="🏁 Ishni tugatdim")],
-        [KeyboardButton(text="📷 Tozalash rasmi yuborish"), KeyboardButton(text="📸 Muammo yuborish")],
-        [KeyboardButton(text="💰 Bonus/Jarimalarim"), KeyboardButton(text="↩️ Menyuga qaytish")]
+        [
+            KeyboardButton(text="🧾 Hisobot yuborish"),
+            KeyboardButton(text="⏰ Ishni boshladim")
+        ],
+        [
+            KeyboardButton(text="🏁 Ishni tugatdim")
+        ],
+        [
+            KeyboardButton(text="📷 Tozalash rasmi yuborish"),
+            KeyboardButton(text="📸 Muammo yuborish")
+        ],
+        [
+            KeyboardButton(text="💰 Bonus/Jarimalarim"),
+            KeyboardButton(text="↩️ Menyuga qaytish")
+        ]
     ]
     return ReplyKeyboardMarkup(
         keyboard=kb,
