dbd777416616c5a76bfa222b8b1338a8f96ae9e9
yabdullox <132588376+yabdullox@users.noreply.github.com>
2025-10-27 17:37:25 +0500

    Update db.py

--- database/db.py
+++ database/db.py
@@ -1,46 +1,49 @@
 import aiosqlite
-
-DB_PATH = "data.db"
+from config import DB_PATH
 
 async def init_db():
     async with aiosqlite.connect(DB_PATH) as db:
-        await db.executescript("""
+        await db.execute("""
         CREATE TABLE IF NOT EXISTS workers (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             name TEXT,
             tg_id INTEGER UNIQUE,
             filial_id INTEGER
-        );
+        )""")
+
+        await db.execute("""
         CREATE TABLE IF NOT EXISTS reports (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             worker_id INTEGER,
-            filial_id INTEGER,
             text TEXT,
             created_at TEXT
-        );
-        CREATE TABLE IF NOT EXISTS bonuses (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            worker_id INTEGER,
-            filial_id INTEGER,
-            reason TEXT,
-            amount INTEGER,
-            created_at TEXT
-        );
-        CREATE TABLE IF NOT EXISTS fines (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            worker_id INTEGER,
-            filial_id INTEGER,
-            reason TEXT,
-            amount INTEGER,
-            created_at TEXT
-        );
+        )""")
+
+        await db.execute("""
         CREATE TABLE IF NOT EXISTS problems (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             worker_id INTEGER,
-            filial_id INTEGER,
-            note TEXT,
             file_id TEXT,
+            note TEXT,
             created_at TEXT
-        );
-        """)
+        )""")
+
+        await db.execute("""
+        CREATE TABLE IF NOT EXISTS bonuses (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            worker_id INTEGER,
+            reason TEXT,
+            amount INTEGER,
+            created_at TEXT
+        )""")
+
+        await db.execute("""
+        CREATE TABLE IF NOT EXISTS fines (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            worker_id INTEGER,
+            reason TEXT,
+            amount INTEGER,
+            created_at TEXT
+        )""")
+
         await db.commit()
