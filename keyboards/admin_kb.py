63645a5167f3d024b2d6fb7375f65c3d5e6c23b5
yabdullox <132588376+yabdullox@users.noreply.github.com>
2025-10-27 17:29:50 +0500

    Update admin_kb.py

--- keyboards/admin_kb.py
+++ keyboards/admin_kb.py
@@ -1,21 +1,8 @@
-from aiogram import types
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 def admin_menu():
-    return types.ReplyKeyboardMarkup(
-        keyboard=[
-            [types.KeyboardButton(text="👷 Ishchilar ro‘yxati"), types.KeyboardButton(text="➕ Ishchi qo‘shish")],
-            [types.KeyboardButton(text="🗑 Ishchini o‘chirish"), types.KeyboardButton(text="💸 Jarima/Bonus yozish")],
-        ],
-        resize_keyboard=True
-    )
-
-# from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-
-# def admin_menu():
-#     kb = [
-#         [KeyboardButton(text="👷 Ishchi qo‘shish"), KeyboardButton(text="❌ Ishchini o‘chirish")],
-#         [KeyboardButton(text="👥 Ishchilar ro‘yxati")],
-#         [KeyboardButton(text="💸 Jarima/Bonus yozish")],
-#         [KeyboardButton(text="↩️ Ortga")]
-#     ]
-#     return ReplyKeyboardMarkup(keyboard=kb, resize_keyboard=True)
+    kb = [
+        [KeyboardButton(text="👷 Ishchilar ro‘yxati"), KeyboardButton(text="➕ Ishchi qo‘shish")],
+        [KeyboardButton(text="🗑 Ishchini o‘chirish"), KeyboardButton(text="💸 Jarima/Bonus yozish")]
+    ]
+    return ReplyKeyboardMarkup(keyboard=kb, resize_keyboard=True)
