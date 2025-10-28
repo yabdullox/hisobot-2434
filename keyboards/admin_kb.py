45b225efc996ccbcd236efc788d6d4fbebb2c43a
yabdullox <132588376+yabdullox@users.noreply.github.com>
2025-10-28 11:41:20 +0500

    Delete keyboards directory

--- keyboards/admin_kb.py
+++
@@ -1,8 +0,0 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-
-def admin_menu():
-    kb = [
-        [KeyboardButton(text="👷 Ishchilar ro‘yxati"), KeyboardButton(text="➕ Ishchi qo‘shish")],
-        [KeyboardButton(text="🗑 Ishchini o‘chirish"), KeyboardButton(text="💸 Jarima/Bonus yozish")]
-    ]
-    return ReplyKeyboardMarkup(keyboard=kb, resize_keyboard=True)
