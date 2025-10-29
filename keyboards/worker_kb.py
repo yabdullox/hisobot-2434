e7b9ca0a49df5cf57100f224394c63b82218f4b2
yabdullox <132588376+yabdullox@users.noreply.github.com>
2025-10-29 13:45:34 +0500

    Update worker_kb.py

--- keyboards/worker_kb.py
+++ keyboards/worker_kb.py
@@ -5,7 +5,7 @@ def get_worker_kb():
     kb = [
         [KeyboardButton(text="🕘 Ishni boshladim"), KeyboardButton(text="🏁 Ishni tugatdim")],
         [KeyboardButton(text="🧹 Tozalash rasmi yuborish"), KeyboardButton(text="💬 Muammo yuborish")],
-        [KeyboardButton(text="💰 Bonus/Jarimalarim")],
+        [KeyboardButton(text="💰 Bonus/Jarimalarim"), KeyboardButton(text="📓 Eslatmalarim")],
         [KeyboardButton(text="⬅️ Menyuga qaytish")]
     ]
     return ReplyKeyboardMarkup(
